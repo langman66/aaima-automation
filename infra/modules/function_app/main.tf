3605b02b9720192c8c5c12ca456fd35aab91fac7
Chris <chris@chrislangston.com>
2025-08-18 18:35:29 -0400

    After completing deploying Function App
    Note, these changes reflect necessary changes in order to access storage accounts.

--- infra/modules/function_app/main.tf
+++ infra/modules/function_app/main.tf
@@ -68,36 +68,64 @@ resource "azurerm_service_plan" "plan" {
 }
 
 resource "azurerm_linux_function_app" "func" {
-  name                       = "func-${var.name_prefix}-wus2"
-  resource_group_name        = var.rg_name
-  location                   = var.location
-  service_plan_id            = azurerm_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.st.name
-  storage_account_access_key = azurerm_storage_account.st.primary_access_key
+  name                          = "func-${var.name_prefix}-wus2"
+  resource_group_name           = var.rg_name
+  location                      = var.location
+  service_plan_id               = azurerm_service_plan.plan.id
+
+  // Keep the storage account name so the platform knows which account to use
+  storage_account_name          = azurerm_storage_account.st.name
+
+  // SWITCH: use managed identity for AzureWebJobsStorage (remove the access key)
+  // was: storage_account_access_key = azurerm_storage_account.st.primary_access_key
+  storage_uses_managed_identity = true
+
   public_network_access_enabled = false
-  https_only                 = true
+  https_only                    = true
 
   identity { type = "SystemAssigned" }
 
   site_config {
-    application_stack { 
-      node_version = "18" 
+    application_stack {
+      node_version = "18"
     }
     vnet_route_all_enabled = true
   }
 
   virtual_network_subnet_id = var.func_integ_subnet_id
 
+  // Identity-based AzureWebJobsStorage configuration
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "SERVICEBUS_NAMESPACE"     = "sb-${var.name_prefix}-wus2.servicebus.windows.net"
+    "WEBSITE_RUN_FROM_PACKAGE"            = "1"
+
+    // Identity-based connection for the default storage:
+    // If you later use a user-assigned identity, add AzureWebJobsStorage__clientId too.
+    "AzureWebJobsStorage__accountName"    = azurerm_storage_account.st.name
+    "AzureWebJobsStorage__blobServiceUri" = azurerm_storage_account.st.primary_blob_endpoint
+    "AzureWebJobsStorage__queueServiceUri"= azurerm_storage_account.st.primary_queue_endpoint
+
+    "SERVICEBUS_NAMESPACE"                = "sb-${var.name_prefix}-wus2.servicebus.windows.net"
   }
 
   depends_on = [
     azurerm_private_endpoint.st_blob,
     azurerm_private_endpoint.st_queue
   ]
+
   tags = var.tags
+}
+
+// Grant the Function’s managed identity access to Blob & Queue data planes
+resource "azurerm_role_assignment" "func_blob_data_contrib" {
+  scope                = azurerm_storage_account.st.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.func.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "func_queue_data_contrib" {
+  scope                = azurerm_storage_account.st.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_linux_function_app.func.identity[0].principal_id
 }
 
 resource "azurerm_private_endpoint" "func_inbound" {
